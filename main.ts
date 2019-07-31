ef7486ff55256a753d0f5c2fe1476036efdf9e08
pfernandes <pfernandes@exagoinc.com>
2019-07-31 17:59:03 -0400

    Successfully hooked up to the Twitter API

--- main.ts
+++ main.ts
@@ -1,38 +1,44 @@
+import dotenv = require("dotenv");
+dotenv.config();
+
 import * as fs from "fs";
 import * as process from "process";
 import { Tweet } from "./tweet";
+import { postTweet } from "./twitter-api";
 
-const key = process.argv[2];
-const infile = process.argv[3];
-if (!key || !infile)
-{
-	console.log(`Usage: node main.js [key] [path-to-file]`);
-	process.exit();
-}
+postTweet();
 
-const fileContent = fs.readFileSync(infile, { encoding: "utf8" });
+// const key = process.argv[2];
+// const infile = process.argv[3];
+// if (!key || !infile)
+// {
+// 	console.log(`Usage: node main.js [key] [path-to-file]`);
+// 	process.exit();
+// }
 
-let part = 0;
-let tweet = new Tweet(`${key}:${part}:`);
-let tweets = [] as Tweet[];
+// const fileContent = fs.readFileSync(infile, { encoding: "utf8" });
 
-const TWITTER_MAX_LEN = 280;
+// let part = 0;
+// let tweet = new Tweet(`${key}:${part}:`);
+// let tweets = [] as Tweet[];
 
-for (let c of fileContent)
-{
-	if (tweet.len + c.length >= TWITTER_MAX_LEN)
-	{
-		tweets.push(tweet);
-		part += 1;
-		tweet = new Tweet(`${key}:${part}:`);
-	}
+// const TWITTER_MAX_LEN = 280;
 
-	tweet.body += c;
-}
+// for (let c of fileContent)
+// {
+// 	if (tweet.len + c.length >= TWITTER_MAX_LEN)
+// 	{
+// 		tweets.push(tweet);
+// 		part += 1;
+// 		tweet = new Tweet(`${key}:${part}:`);
+// 	}
 
-if (tweet.len > 0)
-	tweets.push(tweet);
+// 	tweet.body += c;
+// }
 
-console.log(tweets);
-fs.writeFileSync("out.txt", JSON.stringify(tweets))
+// if (tweet.len > 0)
+// 	tweets.push(tweet);
+
+// console.log(tweets);
+// fs.writeFileSync("out.txt", JSON.stringify(tweets))
 
